128ee1d9892715705575a2edd4509cb5e3557644
Snofts <samuelsnofts@gmail.com>
2025-12-09 15:22:38 +0100

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+                    {label: 'Store', link: '/mac'},
+                    {label: 'Mac', link: '/mac'},
+                    {label: 'Iphone', link: '/mac'},
+                    {label: 'Watch', link: '/mac'},
+                    {label: 'Vision', link: '/mac'},
+                    {label: 'Aitpods', link: '/mac'},
+                ]
