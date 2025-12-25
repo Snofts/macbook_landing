53b25afc7c857e48e78887094853edac78ae4f8f
Snofts <samuelsnofts@gmail.com>
2025-12-25 14:45:13 +0100

    Features page included

--- src/components/models/Macbook.jsx
+++ src/components/models/Macbook.jsx
@@ -8,13 +8,30 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
-import React from "react";
-import { useGLTF, useTexture } from "@react-three/drei";
+import React, { useEffect } from "react";
+import { useGLTF, useTexture, useVideoTexture } from "@react-three/drei";
+import useMacbookStore from "../store";
+import { noChangeParts } from "../../constants";
+import { Color } from "three";
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF("/models/macbook-transformed.glb");
+export default function Model(props) {
+  const {color, texture} = useMacbookStore()
+  const { nodes, materials, scene } = useGLTF("/models/macbook-transformed.glb");
 
-  const texture = useTexture("/screen.png");
+  const screen = useVideoTexture(texture)
+
+   useEffect(() =>{
+    scene.traverse((child) => {
+      if(child.isMesh){
+        // Change color only if the part name is not noChangeParts
+        if(!noChangeParts.includes(child.name)){
+          child.material.color = new Color(color)
+        }
+      }
+    })
+  }, [color, scene])
+
+  // const texture = useTexture("/screen.png");
   return (
     <group {...props} dispose={null}>
       <mesh
@@ -104,10 +121,9 @@ export function Model(props) {
       />
       <mesh
         geometry={nodes.Object_123.geometry}
-        material={materials.sfCQkHOWyrsLmor}
         rotation={[Math.PI / 2, 0, 0]}
       >
-        <meshStandardMaterial map={texture} />
+        <meshBasicMaterial map={screen} />
       </mesh>
       <mesh
         geometry={nodes.Object_127.geometry}
